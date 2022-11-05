63707332b2b4bde7aff3de415643eb58d9b80217
deepak-delta <ddk3857@gmail.com>
2022-11-05 20:09:11 +0530

    Get basic metrics

--- src/utils/metrics.ts
+++ src/utils/metrics.ts
@@ -6,6 +6,13 @@ const app = express()
 const startMerticsServer = () => {
   const collectDefaultMetrics = client.collectDefaultMetrics
 
+  collectDefaultMetrics()
+
+  app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', client.register.contentType)
+    return res.send(await client.register.metrics())
+  })
+
   app.listen(9100, () => {
     console.log(`Metrics Server Started `)
   })
